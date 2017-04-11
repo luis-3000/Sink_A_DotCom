1c3167a8c76fe4b81486066faedaa7801972cc28
Jose Luis Castillo <josecastillocs@gmail.com>
2017-04-11 16:41:14 -0500

    Upgraded version with ArrayList

--- DotCom.java
+++ DotCom.java
@@ -1,32 +1,36 @@
+import java.util.ArrayList;
+
 public class DotCom {
 	
-	int[] locationCells;
+	//int[] locationCells;
+	private ArrayList<String> locationCells; // Improved version with ArrayList<String>
 
 	int numOfHits = 0;
 
-	public void setLocationCells(int[] locs) {
+	public void setLocationCells(ArrayList<String> locs) {
 		locationCells = locs;
 	}
 
-		public String checkYourself(String stringGuess) {
-		int guess = Integer.parseInt(stringGuess); // convert String to an int
-
+	public String checkYourself(String userInput) {
+		
 		String result = "miss";
 
-		for (int cell : locationCells) {
-			if (guess == cell) {
+		// Find out if the uesr guess is in the ArrayList, by asking for its index.
+		// If it's not in the list, then indexOf() returns -1
+		int index = locationCells.indexOf(userInput);
+
+		if (index >= 0) { // If index is greater than or equal to zero, then the 
+						  // user's guess is definately in the list, 
+			locationCells.remove(index); // so, remove it
+
+			if (locationCells.isEmpty()) { // If the list is empty, this was the killing
+				result = "kill";		   // this was the 'killing' blow!
+			} else {
 				result = "hit";
-				numOfHits++;
-				break;
 			}
 		}
 
-		if (numOfHits == locationCells.length) {
-			result = "kill";
-		}
-
-		System.out.println(result);
-
 		return result;
 	}
+
 }
